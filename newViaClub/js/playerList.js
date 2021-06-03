1861360ad0a92634be4cd9551c4b02a2823749dd
Chris309283 <80517220+Chris309283@users.noreply.github.com>
2021-06-03 16:29:43 +0200

    match table

--- newViaClub/js/playerList.js
+++ newViaClub/js/playerList.js
@@ -16,21 +16,21 @@ function showData(xml) {
   var j;
   var xmlDoc = xml.responseXML;
   var table =
-    "<tr><th class='tableStyles'>Name</th><th class='tableStyles'>Number</th><th class='tableStyles'>Positions</th></tr>";
+    "<tr><th>Name</th><th>Number</th><th>Positions</th></tr>";
   var x = xmlDoc.getElementsByTagName("players");
 
   for (i = 0; i < x.length; i++) {
     table +=
-      "<tr><td class='tableStyles'>" +
+      "<tr><td>" +
       x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue +
-      "</td><td class='tableStyles'>" +
+      "</td><td>" +
       x[i].getElementsByTagName("number")[0].childNodes[0].nodeValue +
       "</td>" +
-      "<td class='tableStyles'>";
+      "<td>";
     for (j = 0; j < x[i].getElementsByTagName("positions").length; j++) {
       table +=
         x[i].getElementsByTagName("positions")[j].childNodes[0].nodeValue +
-        ". ";
+        "."+"<br>";
     }
     table += "</td></tr>";
   }
